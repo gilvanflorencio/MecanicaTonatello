ae3cef3344b99a70980eb62880fde6e341a4b99b
GILVANSANTOS <gilvanf28@gmail.com>
2023-10-27 21:39:33 -0300

    topo css

--- js/javascript.js
+++ js/javascript.js
@@ -38,3 +38,23 @@ $(document).ready(function () {
 
 
 
+  document.addEventListener("DOMContentLoaded", function () {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    }
+
+    scrollToTopBtn.onclick = function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    };
+});
